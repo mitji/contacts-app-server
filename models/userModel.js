46ae71de020eb866941b78b7e25ce042c9c48b29
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-30 19:15:42 +0100

    change user schema

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // create userschema
 const userSchema = new Schema({
-  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
   password: {type: String, required: true}
 })
 
