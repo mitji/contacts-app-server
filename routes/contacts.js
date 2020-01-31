a7423f2aaaeaf1227f3facc00f6e42c33474e0e9
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-31 18:18:22 +0100

    add async await to retrieve api data

--- routes/contacts.js
+++ routes/contacts.js
@@ -10,34 +10,22 @@ const {
   validationLoggin,
 } = require('../helpers/middlewares');
 
-// router.get('/', (req, res, next) => {
-//   // call the api
-//   fetch('https://exercise.goldenspear.com/contacts.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     res.status(200).json(data);
-//   })
-//   .catch(err => console.log(err))
-// })
-
-router.get('/', isLoggedIn, /* async */ (req, res, next) => {
+router.get('/', isLoggedIn, (req, res, next) => {
   const token = req.query.token;
 
-  jwt.verify(token, 'supersecret', (err, decoded) => {
+  jwt.verify(token, 'supersecret', async (err, decoded) => {
     if(!err){
-      fetch('https://exercise.goldenspear.com/contacts.json')
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          res.status(200).json(data);
-        })
-        .catch(err => console.log(err))
+      try {
+        const response = await fetch('https://exercise.goldenspear.com/contacts.json');
+        const apiData = await response.json();
+        res.status(200).json(apiData);
+      } catch (error) {
+        console.log(error);
+      }
     } else {
       res.send({error: err});
     }
   })
 })
-
 
 module.exports = router;
